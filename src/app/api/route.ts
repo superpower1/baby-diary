343576fdb56ff47e338401a674fb1943de4aa7ba
superpower1 <haofu.zhu@gmail.com>
2024-02-11 21:34:23 +1100

    feat: add comments id api

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,23 @@
-export const GET = () => {
-  return new Response('Hello world!')
+import { cookies, headers } from "next/headers";
+import { NextRequest } from "next/server"
+
+export const GET = (request: NextRequest) => {
+  const requestHeaders = new Headers(request.headers);
+  const headerList = headers();
+
+  cookies().set('resultsPerPage', '20')
+  const theme = request.cookies.get('theme')
+
+  console.log(requestHeaders.get('Authorization'));
+  console.log(headerList.get('Authorization'));
+
+  console.log(theme)
+  console.log(cookies().get('resultsPerPage'))
+
+  return new Response('<h1>Hello world!</h1>', {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark"
+    }
+  })
 }
